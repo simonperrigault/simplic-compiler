9e133a4dc43ceabc411b4808f668deb104c8e10f
Simon Perrigault <simonperrigault22@gmail.com>
2024-09-07 19:06:59 +0200

    more tests, scanner and parser seem to work

--- A1/src/test/lang/ParseTests.java
+++ A1/src/test/lang/ParseTests.java
@@ -12,12 +12,12 @@ public class ParseTests {
 	/** Directory where the test input files are stored. */
 	private static final File TEST_DIRECTORY = new File("testfiles");
 
-	@Test public void smallestTest() {
-		Util.testValidSyntax(TEST_DIRECTORY, "smallest.in");
-	}
-
 	@Test public void assignmentTest() {
 		Util.testValidSyntax(TEST_DIRECTORY, "assignment.in");
+	}
+
+	@Test public void not_assignTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "not_assign.in");
 	}
 
 	@Test public void forTest() {
@@ -30,6 +30,10 @@ public class ParseTests {
 
 	@Test public void if_notTest() {
 		Util.testValidSyntax(TEST_DIRECTORY, "if_not.in");
+	}
+
+	@Test public void imbricatedIfTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "imbricatedIf.in");
 	}
 
 	@Test public void allTest() {
