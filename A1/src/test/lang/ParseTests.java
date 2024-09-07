b0dce81668a760cbc8c60d31455a5180aec95e8d
Simon Perrigault <simonperrigault22@gmail.com>
2024-09-07 18:42:49 +0200

    more tests and change parseTest

--- A1/src/test/lang/ParseTests.java
+++ A1/src/test/lang/ParseTests.java
@@ -12,31 +12,27 @@ public class ParseTests {
 	/** Directory where the test input files are stored. */
 	private static final File TEST_DIRECTORY = new File("testfiles");
 
-	@Test public void identifier() {
-		Util.testValidSyntax(TEST_DIRECTORY, "identifier.in");
+	@Test public void assignmentTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "assignment.in");
 	}
 
-	@Test public void let() {
-		Util.testValidSyntax(TEST_DIRECTORY, "let.in");
+	@Test public void forTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "for.in");
 	}
 
-	@Test public void numerical() {
-		Util.testValidSyntax(TEST_DIRECTORY, "numerical.in");
+	@Test public void ifTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "if.in");
 	}
 
-	@Test public void product() {
-		Util.testValidSyntax(TEST_DIRECTORY, "product.in");
+	@Test public void if_notTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "if_not.in");
 	}
 
-	@Test public void error() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error.in");
+	@Test public void allTest() {
+		Util.testValidSyntax(TEST_DIRECTORY, "all.in");
 	}
 
-	@Test public void error2() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error2.in");
-	}
-
-	@Test public void error3() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error3.in");
+	@Test public void errAssignmentTest() {
+		Util.testSyntaxError(TEST_DIRECTORY, "errAssignment.in");
 	}
 }
