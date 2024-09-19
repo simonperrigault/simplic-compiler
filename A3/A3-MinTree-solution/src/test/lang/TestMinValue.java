180a19368c22bdca9bf1c073cf0e8cac946bc0da
Simon Perrigault <simonperrigault22@gmail.com>
2024-09-19 17:24:04 +0200

    attribute grammar

--- A3/A3-MinTree-solution/src/test/lang/TestMinValue.java
+++ A3/A3-MinTree-solution/src/test/lang/TestMinValue.java
@@ -67,4 +67,16 @@ public class TestMinValue {
         assertEquals(1, p.nbrOfMinValues());
     }
 
+    @Test
+    public void nbrOfMinValues2() {
+        Leaf l1 = new Leaf(1);
+        Leaf l2 = new Leaf(1);
+        Leaf l3 = new Leaf(3);
+        Pair p1 = new Pair(l2, l3);
+        Pair p2 = new Pair(l1, p1);
+        Program p = new Program(p2);
+        
+        assertEquals(2, p.nbrOfMinValues());
+    }
+
 }
