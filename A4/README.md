eaefdfbcc4e7a8067a56ddcd67ef155a8d56ca54
Simon Perrigault <simonperrigault22@gmail.com>
2024-09-25 15:49:59 +0200

    copy A3 into A4

--- A4/README.md
+++ A4/README.md
@@ -1,4 +1,10 @@
 # Assignment A4
 
+_Verify exception thrown_
 
+write "syn int IdDecl.a() = a() + 1" and add "if (getBinding(i).getIdDecl().a() == 0) to compute and get error
 
+_value of decl attribute_
+UnknownDecl
+
+then decome IdDecl
---
+++ A4/src/test/lang/TestNameAnalysis.java
@@ -0,0 +1,42 @@
+package lang;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import lang.ast.Program;
+
+/**
+ * This is a parameterized test: one test case is generated for each input
+ * file found in TEST_DIRECTORY. Input files should have the ".in" extension.
+ */
+@RunWith(Parameterized.class)
+public class TestNameAnalysis {
+    /** Directory where the test input files are stored. */
+    private static final File TEST_DIRECTORY = new File("testfiles/nameanalysis");
+
+    private final String filename;
+    public TestNameAnalysis(String testFile) {
+        filename = testFile;
+    }
+
+    @Test public void runTest() throws Exception {
+        Program program = (Program) Util.parse(new File(TEST_DIRECTORY, filename));
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        program.checkNames(new PrintStream(bytes));
+        String actual = bytes.toString();
+        Util.compareOutput(actual,
+                new File(TEST_DIRECTORY, Util.changeExtension(filename, ".out")),
+                new File(TEST_DIRECTORY, Util.changeExtension(filename, ".expected")));
+    }
+
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> getTests() {
+        return Util.getTestParameters(TEST_DIRECTORY, ".in");
+    }
+}
