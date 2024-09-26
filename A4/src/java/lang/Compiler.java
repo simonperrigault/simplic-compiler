744327e9a379b43ce451777ec6e3536eb6721469
Simon Perrigault <simonperrigault22@gmail.com>
2024-09-26 11:13:07 +0200

    variable and func namespace

--- A4/src/java/lang/Compiler.java
+++ A4/src/java/lang/Compiler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 
 import lang.ast.Program;
+import lang.ast.ErrorMessage;
 import lang.ast.LangParser;
 import lang.ast.LangScanner;
 
@@ -36,7 +37,15 @@ public class Compiler {
             Program program = parse(args);
             // program.dumpTree(System.out);
             // System.out.println("MSN : " + MSNVisitor.result(program));
-            program.prettyPrint(System.out);
+            // program.prettyPrint(System.out);
+            if (!program.errors().isEmpty()) {
+                System.err.println();
+                System.err.println("Errors: ");
+                for (ErrorMessage e: program.errors()) {
+                    System.err.println("- " + e);
+                }
+                System.exit(1);
+            }
             
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
